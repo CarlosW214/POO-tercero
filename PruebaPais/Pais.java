6caf2a8e9ba034817444bf89c388db5fb6a68561
CarlosW214 <carlos4562111@gmail.com>
2023-10-01 21:03:12 -0600

    T3 2/4

--- PruebaPais/Pais.java
+++ PruebaPais/Pais.java
@@ -15,7 +15,7 @@ public class Pais {
     public void setContinente(Continente continente) {
         this.continente = continente;
     }
-    
+
     public float getPIB() {
         return PIB;
     }
@@ -57,22 +57,36 @@ public class Pais {
 
     public void DarPIB(){
 
-        System.out.println("\nEl pib de "+getOfficialName()+"es :"+getPIB());
+        System.out.println("\nEl pib de "+getOfficialName()+" es :"+getPIB());
 
     }
 
     public int size(int size){
         
         size=getSuperficie()+4;
-        System.out.println("La superfice de "+getName()+"es :"+size);
+        System.out.println("La superfice de "+getName()+" es :"+size);
         return size;
 
     }
 
     public int population(int population){
         population=getPoblacion();
-        System.out.println("La poblacion del "+getName()+"es: "+population);
+        System.out.println("La poblacion del "+getName()+" en millones es: "+population);
         return population;
 
     }
+
+    public void datosPais(){
+        System.out.println("\nEl nombre oficial es: "+getOfficialName()+"\nEl nombre comun es: "+getName()+"\nEl PIB de "+getName()+"es: "+getPIB()+"\nLa superfice de "+getName()+"es:"+getSuperficie()+"\nSe encuentra en: "+getContinente());
+    
+    }
+
+    public int rank(int rank){
+        rank=getRanking();
+        System.out.println("El ranking de "+getName()+"es: "+getRanking());
+        return rank;
+
+    }
+
 }
+
--- PruebaPais/PruebaPais.java
+++ PruebaPais/PruebaPais.java
@@ -8,6 +8,60 @@ public class PruebaPais{
 
         System.out.println("Programa que señala atributos de algunas naciones");
 
+        Mexico.setContinente(Continente.AMERICA);
+        Mexico.setName("Mexico");
+        Mexico.setOfficialName("Estados Unidos Mexicanos");
+        Mexico.setPoblacion(128);
+        Mexico.setPIB(1322740);
+        Mexico.setRanking(13);
+        Mexico.setSuperficie(1964375);
+
+        Mexico.DarPIB();
+        Mexico.size(12);
+        Mexico.population(1);
+        Mexico.datosPais();
+        Mexico.rank(13);
+
+
+        Polonia.setContinente(Continente.EUROPA);
+        Polonia.setName("Polonia");
+        Polonia.setOfficialName("Republica de Polonia");
+        Polonia.setPoblacion(41026068);
+        Polonia.setPIB(642121);
+        Polonia.setRanking(20);
+        Polonia.setSuperficie(312696);
+
+        Polonia.DarPIB();
+        Polonia.size(12);
+        Polonia.population(23);
+        Polonia.datosPais();
+        Polonia.rank(23);
+
         
+        URRS.setContinente(Continente.EUROPA);
+        URRS.setName("Union Sovietica");
+        URRS.setOfficialName("Union de Republicas Sovieticas Socialistas");
+        URRS.setPoblacion(293047571);
+        URRS.setPIB(100585860);
+        URRS.setRanking(1);
+        URRS.setSuperficie(22402200);
+
+        URRS.datosPais();
+        URRS.DarPIB();
+        URRS.rank(1);
+
+        Argentina.setContinente(Continente.AMERICA);
+        Argentina.setName("Argentina");
+        Argentina.setOfficialName("Republica de Argentina");
+        Argentina.setPoblacion(46044703);
+        Argentina.setPIB(630689);
+        Argentina.setRanking(24);
+        Argentina.setSuperficie(2780400);
+
+        Argentina.datosPais();
+        Argentina.DarPIB();
+
+        
+
     }
 }
