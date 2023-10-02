d06312021254b10e423a1b7fe32fd3da80347f7b
Carlos Lerdo <carlos4562111@gmail.com>
2023-10-02 11:21:18 -0600

    Ejercicio arreglos

--- PruebaArreglos/PruebaArreglos.java
+++ PruebaArreglos/PruebaArreglos.java
@@ -1,25 +1,30 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+import java.util.Random;
 
-/**
- *
- * @author tania
- */
 public class PruebaArreglos {
     public static void main(String[] args) {
         //Se crea un arreglo de 5 enteros llamado edades
         //int Edades[5]; Sintaxis para lenguaje C
+        
+        // Creando Numeros aleatorios. 
+
+        Random rand =new Random();
+        int n=rand.nextInt(81);
+        n+=1;
+
+
         int Edades[]= new int[5];
         //Guardando en el primer entero de Edades el valor 10
         Edades[0]=10;
         Edades[1]=1995;
         Edades[2]=(int)(Math.random()*100); //Hacer un cast
-        System.out.println(Edades[2]);
+       
+        //System.out.println(Edades[2]);
+        
         Edades[3]=13;
         Edades[4]=9;
-        //Edades[5]=0;
+        
+        int[] edad;
+        edad=new int[40];
         
         //Creando un arreglo de 5 números flotantes llamado Pesos
         //Indica que es un vector
@@ -27,17 +32,53 @@ public class PruebaArreglos {
         
         //Inicializando el arreglo Pesos con valores aleatorios comprendidos entre 
         //1 y 100 mediante el uso de un for
-        for(int i=0;i<5;i++){
-            Pesos[i]=(float)(Math.random()*10);
-            System.out.println(Pesos[i]);
+        for(int i=0;i<2;i++){
+            Pesos[i]=(float)(Math.random());
+            System.out.println("Peso "+i+":"+Pesos[i]+"\n\n");
         }
         
+        for(int i=0; i<edad.length; i++){
+
+            edad[i]=rand.nextInt(40);
+            System.out.println("Edad "+i+":"+edad[i]);
+        }
+        
+
+
         //Creando u  arreglo de 5 caracteres llamado Letras que representen una letra del abecedario
-        char Letras[]= new char[5];
-        String nombre="Tania"+"García"+"Díaz";
+        //Generando Nombres de forma aleatoria.
+        //char Letras[]= new char[5];
+        
+        String nombres[]={"Tania","Claudia","Ojeda","Teresa","Carlos","AMLO","Auriel","Christian"}; //0-7
+        String apellidos[]={"Reyes","Paz","Diaz","Perez","Maldonado","Rojas","Baez","Prieto"};
+        String apellidosma[]={"Crisostomo","Zarco","Perez","Kenny","Panda","LoboGris","Leoner","Mendez"};
+        
+        //Definiendo tres numeros enteros que se usaran como indice.
+       /*  int In;  //Indice para nombre.
+        int Ip;  //Indice para apellido.
+        int Im;  //Indicde para mpa.
+        */
+        String nombreCompleto;
+
+        for(int i=0; i<5;i++){
+        
+            Random rand1 =new Random();
+            int n1=rand1.nextInt(7);
+            n1+=1;
+            int n2=rand1.nextInt(7);
+            n2+=1;
+            int n3=rand1.nextInt(7);
+            n3+=1;
+            
+                nombreCompleto=nombres[n1]+" "+apellidos[n2]+" "+apellidosma[n3];
+                System.out.println(nombreCompleto);
+        }
+
         String Nombres[] = new String[5];
         Nombres[1]="Pedro";
         Nombres[0]="Juan";
         Nombres[4]="Karen Madrigal";
+
+
     }
 }
