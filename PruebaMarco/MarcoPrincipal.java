d20c9b806c3dbefd1a490a8c0ca7bdfc9e5adfea
Carlos Lerdo <carlos4562111@gmail.com>
2023-11-29 10:46:21 -0600

    MARCOS

---
+++ PruebaMarco/MarcoPrincipal.java
@@ -0,0 +1,17 @@
+import javax.swing.*;
+
+public class MarcoPrincipal extends JFrame{ 
+    //Agregando Componentes y atributos.
+    JButton bt1;
+
+    //Agregando comportamientos o metodos.
+    public MarcoPrincipal() {
+        super();
+
+    //Definir el tamaño del marco con setSizw.
+    this.setSize(600, 500);
+    //Definiendo el modo de funcionamiento del marco.
+    
+
+    }   
+}
---
+++ PruebaMarco/PruebaMarco.java
@@ -0,0 +1,8 @@
+public class PruebaMarco {
+    public static void main(String[] args) {
+        //Creando un marco principal.
+
+        MarcoPrincipal MP1 = new MarcoPrincipal();
+        
+    }
+}
